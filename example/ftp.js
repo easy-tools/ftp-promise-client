90851a8353ba52f2537fef4369b591317b738db1
liuwill <liuwei_will@qq.com>
2018-11-05 15:14:25 +0800

    create ftp client example

---
+++ example/ftp.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var path = require('path')
+
+var FtpClientPromise = require('../lib')
+
+var rawPath = './app.json'
+var configPath = path.join(__dirname, rawPath)
+if (!fs.existsSync(configPath)) {
+  throw new Error('app.json Config is not exist')
+}
+let appConfig = require(rawPath)
+
+var ftpClient = FtpClientPromise.Factory(appConfig.ftp)
+ftpClient.list().then(function (result) {
+  console.log(result.response)
+})
